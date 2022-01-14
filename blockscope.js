635e3517226cd80a838b1267cdd29a3a4673f609
向山　翔 <hms0175km@gmail.com>
2022-01-14 12:59:41 +0900

    [Add]関数スコープとブロック関数

---
+++ blockscope.js
@@ -0,0 +1,15 @@
+function a() {
+    let b = 0;
+    console.log(b);
+}
+a();
+
+if(true) {
+    var c = 1
+
+    var d =function() {
+        console.log('d is called');
+    }
+}
+d();
+console.log(d)
