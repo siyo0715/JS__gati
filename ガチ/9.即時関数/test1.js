ba2ade3dd4a4f6466c4d254ef2e24102800bb00d
向山　翔 <hms0175km@gmail.com>
2022-01-14 15:54:38 +0900

    [Add]即時関数

---
+++ ガチ/9.即時関数/test1.js
@@ -0,0 +1,37 @@
+const calc = (function(val) {
+    return {
+        plus: function (target) {
+            const newVal = val + target;
+            // console.log(val + "+" + target + "=" + newVal)
+            console.log(`${val} + ${target} = ${newVal}`)
+            val = newVal;
+        },
+        minus: function (target) {
+            const newVal = val + target;
+            // console.log(val + "-" + target + "=" + newVal)
+            console.log(`${val} - ${target} = ${newVal}`)
+            val = newVal;
+        },
+        multiply: function (target) {
+            const newVal = val + target;
+            // console.log(val + "*" + target + "=" + newVal)
+            console.log(`${val} * ${target} = ${newVal}`)
+            val = newVal;
+        },
+        divide: function (target) {
+            const newVal = val + target;
+            // console.log(val + "/" + target + "=" + newVal)
+            console.log(`${val} / ${target} = ${newVal}`)
+            val = newVal;
+        }
+
+
+    };
+})(10);
+
+
+
+calc.plus(5);
+calc.minus(3);
+calc.multiply(3);
+calc.divide(2);
