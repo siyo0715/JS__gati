e62a27629609569fa9af20eeb76de8b2f0847075
向山　翔 <hms0175km@gmail.com>
2022-01-15 15:50:17 +0900

    [Add]関数

--- data.js
+++ data.js
@@ -24,3 +24,16 @@ const e = {
 //e = {}
 e.prop = 'bye'
 console.log(b)
+
+// 関数の参照
+
+let f = 0;
+
+function fn1(arg1) {
+    arg1 = 1;
+    console.log(f.arg1)
+}
+// let arg1 = f;
+// arg1 = 1;
+// console.log(a, arg1)
+fn1(f);
