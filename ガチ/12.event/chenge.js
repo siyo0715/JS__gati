345bc9d061cf2105fff3dd2754389ca9459d5616
向山　翔 <hms0175km@gmail.com>
2022-01-16 16:10:57 +0900

    [Add]event

---
+++ ガチ/12.event/chenge.js
@@ -0,0 +1,4 @@
+document.querySelector('#aaa').innerHTML='bbb';
+        document.querySelector("#btn1").addEventListener('click', ()=>{
+            document.querySelector("#aaa").innerHTML = "ccc";
+        })
